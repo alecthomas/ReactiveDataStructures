4e3009dddf2ad1546e5c443ceff80e73687bbe94
Alec Thomas <alec@swapoff.org>
2015-09-13 18:01:18 +1000

    Initial Commit

---
+++ ReactiveDataStructures/ReactiveDataStructures.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveDataStructures.h
+//  ReactiveDataStructures
+//
+//  Created by Alec Thomas on 13/09/2015.
+//  Copyright © 2015 SwapOff. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReactiveDataStructures.
+FOUNDATION_EXPORT double ReactiveDataStructuresVersionNumber;
+
+//! Project version string for ReactiveDataStructures.
+FOUNDATION_EXPORT const unsigned char ReactiveDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveDataStructures/PublicHeader.h>
+
+
